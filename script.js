3d3597d28f69de16edaddd50dc5dd43dfdca7013
elena_e <elenaka123@gmail.com>
2024-03-21 12:56:30 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var applesBananas = 5 + 8;
+console.log(applesBananas);
+
+console.log(100 / 2);
+
+var mealBudget = 30.36;
+var mealCost = 10.25;
+console.log(`You have $${mealBudget - mealCost} remaining.`);
+
+var tvShows = Number("23");
+var movies = 12;
+console.log(tvShows + movies);
+
+var favoriteGenre = prompt("What's your favorite music  genre?");
+console.log(favoriteGenre);
+
+var oldFunds = 1500;
+var newFunds = Number(prompt("How much funds were raised?"));
+console.log(
+    `The fundraiser total is now $${oldFunds + newFunds}!.`
+);
